4583fe6fa6e915fb7a9b6367f991c062d171a86e
TheGodlessOne <alexnikv@mail.ru>
2020-03-01 21:17:18 +0300

    moved back 'index'

--- responce/urls.py
+++ responce/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from . import views
 
-# urlpatterns = [
-#     path('', views.index, name='index'),
-# ]
+def index(request):
+    return render(request, 'index.html')
+    
