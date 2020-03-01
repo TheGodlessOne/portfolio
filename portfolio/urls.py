f822a4a11290a0936abdfdb610e63cfd05c28f58
TheGodlessOne <alexnikv@mail.ru>
2020-03-01 20:31:42 +0300

    checked

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('responce/', include('responce.urls')),
+    path('index/', include('responce.urls')),
 ]
