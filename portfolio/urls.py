d489e5b4ec5317013063b4fb9b6159708e46c8af
TheGodlessOne <alexnikv@mail.ru>
2020-03-01 20:50:26 +0300

    added index page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('responce.urls')),
+    path('index/', views.index, name='index')
+    path('responces/', include('responce.urls')),
 ]
