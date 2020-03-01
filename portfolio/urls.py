36b08b72286a91df8cf017d61d5294d5ff4ef9e6
TheGodlessOne <alexnikv@mail.ru>
2020-03-01 21:28:14 +0300

    index (again)

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from responce.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('responce.urls')),
+    path('', index),
 ]
