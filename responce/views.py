a263c3d384293093ba7fe54ef298ec1db708cc97
TheGodlessOne <alexnikv@mail.ru>
2020-03-02 03:25:52 +0300

    added view for projects

--- responce/views.py
+++ responce/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def projects(request): 
+    return render(request, 'projects.html')
