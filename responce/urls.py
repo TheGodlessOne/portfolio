392cce597b187dfad99f965712ce2a66b4cb3983
TheGodlessOne <alexnikv@mail.ru>
2020-03-02 16:39:02 +0300

    moved target page of 'project' on 'projects.html'

--- responce/urls.py
+++ responce/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.project_all, name='projects'),
+    path('<int:pk>/', views.project_detail, name='project_detail'),
+]
