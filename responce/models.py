be86910a3ddd3e6e294a27f365c5f15b08c0bfcd
TheGodlessOne <alexnikv@mail.ru>
2020-03-02 03:45:53 +0300

    added Project model

--- responce/models.py
+++ responce/models.py
@@ -5,3 +5,10 @@ class Responce(models.Model):
     email = models.EmailField()
     site_url = models.URLField()
     comment = models.TextField(max_length = 240)
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.FilePathField(path="/img")
+    
