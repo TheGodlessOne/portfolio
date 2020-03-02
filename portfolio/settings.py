8c65e07c49ed3eeaa7aadd4e4bbbb3550ad47f6b
TheGodlessOne <alexnikv@mail.ru>
2020-03-02 03:21:57 +0300

    edited the base templates url

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'responce/templates'), 
-                os.path.join(BASE_DIR, 'portfolio/templates')
+                os.path.join(BASE_DIR, 'templates')
                 ],
         'APP_DIRS': True,
         'OPTIONS': {
