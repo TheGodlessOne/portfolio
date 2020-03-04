e02c6204d64b0749cab2c378b5e90cb51bf77a66
TheGodlessOne <alexnikv@mail.ru>
2020-03-04 12:01:51 +0300

    changed 'p_key' to 'pk'

--- responce/urls.py
+++ responce/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.project_all, name='projects'),
-    path('<int:p_key>/', views.project_detail, name='project_detail'),
+    path("", views.project_all, name='projects'),
+    path("<int:pk>/", views.project_detail, name='project_detail'),
 ]
