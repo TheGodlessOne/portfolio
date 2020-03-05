5887e936ca9bf58b9456aac6f884f1db243d7191
TheGodlessOne <alexnikv@mail.ru>
2020-03-05 10:22:41 +0300

    modified 'contact' view into 'contact_form'

--- responce/views.py
+++ responce/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from responce.models import Project
+from responce.forms import Responce
+from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_protect
 
 
 # Create your views here.
@@ -21,5 +24,18 @@ def project_detail(request, pk):
     }
     return render(request, 'project_detail.html', context)
 
-def contacts(request): 
-    return render(request, 'contacts.html')
+@cache_page(60 * 15)
+@csrf_protect
+def contact_form(request):
+    if request.method == 'POST':
+        form = Responce(request.POST)
+        if form.is_valid():
+            name = form.fields['name']
+            email = form.fields['email']
+            site_url = form.fields['site_url']
+            comment = form.fields['comment']
+            instances = form.save()
+            return render(request, 'contacts.html', {'form': form})
+    else:
+        form = Responce()
+    return render(request, 'contacts.html', {'form': form})
