e6632aa233ef316cc9044ffb0c9641589ad8de15
TheGodlessOne <alexnikv@mail.ru>
2020-03-05 07:51:59 +0300

    added index page url

--- responce/urls.py
+++ responce/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('index.html', views.index, name='index'),
     path('projects/', views.project_all, name='projects'),
     path('projects/<int:pk>/', views.project_detail, name='project_detail'),
 ]
